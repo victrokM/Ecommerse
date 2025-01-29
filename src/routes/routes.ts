813a9c4e79630edf50e212804ca9ddb73375aeb4
nat2698 <nata.salazar2698@gmail.com>
2025-01-29 10:41:34 -0500

    add change

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,10 @@ import routerProducts from "./products.routes";
 import health from "./health.routes";
 import ShoppingCart from "./shoppingCart.routes";
 import user from "./user.routes";
+import address from "./address.routes";
+import category from "./category.routes";
+import stores from "./stores.routes";
 
-const routes = [routerProducts, health, ShoppingCart, user];
+const routes = [routerProducts, health, ShoppingCart, user, address, category, stores];
 
 export default routes;
